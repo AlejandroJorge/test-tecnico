c71cc5b536f23e89d57d6c57e761a160904a4750
AlejandroJorge <a20202060@pucp.edu.pe>
2024-03-04 15:00:49 -0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import FormPage from "./FormPage";
 import TablePage from "./TablePage";
 
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { Menubar } from "primereact/menubar";
 
 const router = createBrowserRouter([
   { path: "/", element: <MainPage /> },
@@ -21,6 +22,14 @@ function App() {
     <>
       <PrimeReactProvider>
         <QueryClientProvider client={queryClient}>
+          <Menubar
+            model={[
+              { label: "Home", command: () => router.navigate("/") },
+              { label: "Form", command: () => router.navigate("/form") },
+              { label: "Table", command: () => router.navigate("/table") },
+            ]}
+            style={{ marginBottom: "2rem", justifyContent: "center" }}
+          />
           <RouterProvider router={router} />
         </QueryClientProvider>
       </PrimeReactProvider>
